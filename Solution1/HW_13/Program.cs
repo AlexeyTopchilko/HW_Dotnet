91e36a6d10ce3274d481144a6a9e5e58f083bb0d
User <User@USER->
2021-03-25 22:07:04 +0300

    Added Newtonsoft.Json package

---
+++ Solution1/HW_13/Program.cs
@@ -0,0 +1,37 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.IO;
+
+namespace HW_13
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            StreamWriter sw = new StreamWriter("Songs.json", true);
+            Song song = new Song("Hooked on a feeling", "Blue Swede", 1992, 3, 7);
+            Song song1 = new Song("B.Y.O.B.", "SOAD", 2005, 4, "rock");
+            Song song2 = new Song("Kolschik", "Misha Krug", 1994, 5, "5");
+
+            var songInfo = Song.GetSongData(song);
+            var song1Info = Song.GetSongData(song1);
+            var song2Info = Song.GetSongData(song2);
+            
+            List<dynamic> collection = new List<dynamic>
+            {
+                songInfo,
+                song1Info,
+                song2Info
+            };
+
+            foreach (var item in collection)
+            {
+                string json = JsonConvert.SerializeObject(item, Formatting.Indented);
+                Console.WriteLine(json);
+                sw.Write(json);
+            }
+            sw.Close();
+        }
+    }
+}
---
+++ Solution1/HW_13/Song.cs
@@ -0,0 +1,103 @@
+﻿using System;
+
+namespace HW_13
+{
+    class Song
+    {
+        public string Title { get; set; }
+
+        public int Minutes { get; set; }
+
+        public string Author { get; set; }
+
+        public int Year { get; set; }
+
+        public Genres Genre { get; set; }
+
+
+        public enum Genres
+        {
+            Unknown,
+            Pop,
+            Classic,
+            Rock,
+            Jazz,
+            Chanson
+        }
+        static public int Length = Enum.GetNames(typeof(Genres)).Length;
+
+        static public dynamic GetSongData(Song song)
+        {
+            var songsData = new
+            {
+                Title = song.Title,
+                Author = song.Author,
+                Minutes = song.Minutes,
+                Genre = song.Genre
+            };
+
+            return songsData;
+        }
+        public Song(string title, string author, int year, int minutes, int genre)
+        {
+            Title = ValidateTitleOrAuthor(title);
+            Author = ValidateTitleOrAuthor(author);
+            Year = ValidateYear(year);
+            Minutes = ValidateMinutes(minutes);
+            if (genre >= 0 && genre <= Length - 1)
+                Genre = (Genres)genre;
+            else
+                Genre = default;
+        }
+
+        public Song(string title, string author, int year, int minutes, string genre)
+        {
+            Title = ValidateTitleOrAuthor(title);
+            Author = ValidateTitleOrAuthor(author);
+            Year = ValidateYear(year);
+            Minutes = ValidateMinutes(minutes);
+            Genre = ValidateGenre(genre);
+        }
+
+        private static Genres ValidateGenre(string genre)
+        {
+            if (genre == null)
+                return Genres.Unknown;
+            else if (Int32.TryParse(genre, out int intGenre))
+            {
+                if (intGenre > Length - 1 || intGenre < 0)
+                    return Genres.Unknown;
+                return (Genres)Int32.Parse(genre);
+            }
+            else
+            {
+                bool chek = Enum.TryParse(genre, true, out Genres currentGenre);
+                return currentGenre;
+            }
+        }
+
+        private static int ValidateYear(int year)
+        {
+            if (year > DateTime.Now.Year)
+                return DateTime.Now.Year;
+            else
+                return year;
+        }
+
+        private static string ValidateTitleOrAuthor(string name)
+        {
+            if (name.Equals(string.Empty) || name == null)
+                return "Unknown";
+            else
+                return name;
+        }
+
+        private static int ValidateMinutes(int minutes)
+        {
+            if (minutes < 0)
+                return 0;
+            else
+                return minutes;
+        }
+    }
+}
